1ec7ded58cd23bd419203d59909abf6dfc70e28b
Sabrina Williams <barkimedes@gmail.com>
2015-10-30 12:30:52 -0400

    The test script now installs and runs tthe tests. It (as with most other things I've tried) works on my machine. So I'll give that a whirl.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -12,6 +12,8 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
+python setup.py install --user
+
 echo 'python/tests/checkers_test.py'
 python python/tests/checkers_test.py
 
